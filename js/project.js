833f0a67314dc43028b566ec93c15c130b3e45dc
Wadim Lavrentev <aleksandrovichvl@gmail.com>
2024-10-20 12:19:55 +0500

    Change

--- js/project.js
+++ js/project.js
@@ -1,11 +1,20 @@
 let tooltipElem;
 
-document.onmouseover = function(event) {
+document.onclick = function(event) {
   let target = event.target;
 
-  // если у нас есть подсказка...
+  if (!((event.target.closest('.hint')) || (event.target.closest('.meaning')) || (event.target.closest('.tooltip')))){
+    tooltipElem.remove();
+    tooltipElem = '';
+  }else{
+    func();
+  }
+
+  function func() {
+    // если у нас есть подсказка...
   let tooltipHtml = target.dataset.tooltip;
   if (!tooltipHtml) return;
+  
 
   // ...создадим элемент для подсказки
 
@@ -16,14 +25,14 @@ document.onmouseover = function(event) {
 
   // спозиционируем его сверху от аннотируемого элемента (top-center)
   let coords = target.getBoundingClientRect();
-  let tooltipCoords = tooltipElem.getBoundingClientRect();
 
   let elem = document.getElementById('content');
 
- let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;  
-  
-  if (left <= 0) left = 0; // не заезжать за левый край окна
-  
+  // let right = elem.offsetWidth - tooltipElem.offsetWidth;
+  let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+  if (left < 0) left = 0; // не заезжать за левый край окна
+  if (tooltipElem.right == 0) left = (elem.offsetWidth - tooltipElem.offsetWidth)-8; // не заезжать за правый край окна
+
   let top = coords.top - tooltipElem.offsetHeight - 5;
   if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
     top = coords.top + target.offsetHeight + 5;
@@ -31,16 +40,15 @@ document.onmouseover = function(event) {
 
   tooltipElem.style.left = left + 'px';
   tooltipElem.style.top = top + 'px';
-};
-
-document.onmouseout = function(e) {
-
-  if (tooltipElem) {
-    tooltipElem.remove();
-    tooltipElem = null;
   }
-
+  
 };
+
+
+
+ 
+
+
 
 
 
